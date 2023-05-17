84d908058a9ccae6fa3dc45420a1d86445a010be
metrikspacex-resume <metrikspacex.resume@gmail.com>
2023-05-17 06:21:26 -0400

    screenshots added

--- src/components/result-summary/results-summary.tsx
+++ src/components/result-summary/results-summary.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable id-length */
 import clsx from "clsx";
 import type { HTMLAttributes } from "react";
 
+import type { Data } from "../../types/Data";
 import styles from "./results-summary.module.scss";
 import SummaryItem from "./summary-item/summary-item";
 
 export type ResultsSummaryProps = HTMLAttributes<HTMLDivElement>;
 export default function ResultsSummary({ className }: ResultsSummaryProps) {
-  const data = [
+  const data: Data = [
     {
       category: "Reaction",
       icon: "/react-fem-results-summary-component/images/icon-reaction.svg",
@@ -28,12 +30,19 @@ export default function ResultsSummary({ className }: ResultsSummaryProps) {
       score: 72,
     },
   ];
+
+  const average = Math.floor(
+    data
+      .filter((data) => data.score)
+      .reduce((x, current) => x + current.score, 0) / data.length
+  );
+
   return (
     <div className={clsx(styles.root, className)}>
       <div className={clsx(styles.results)}>
         <h1>Your Results</h1>
         <div>
-          <span>76</span>
+          <span>{average}</span>
           <span>of 100</span>
         </div>
         <div>
